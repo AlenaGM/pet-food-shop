583c38086cec5d44c10d6fa7382f7b582ba9a84d
Alena Guillaume <91268509+AlenaGM@users.noreply.github.com>
2021-12-23 02:16:45 +0100

    fixed faq spoiler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu,.header').toggleClass('active');
-        $('.body').toggleClass('lock');
-    });
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu,.header').toggleClass('active');
+    $('.body').toggleClass('lock');
+  });
 });
-
-$(document).ready(function() {
-  $('.spoiler__minisubtitle').click(function(event) {
-  if($('.spoiler').hasClass('open')){
-    $('.spoiler__minisubtitle').not($(this)).removeClass('active');
-    $('.spoiler__text').not($(this).next()).slideUP(300);
-  }
-  $(this).toggleClass("active").next().slideToggle(300);
-});
-});
-
-
 
 $(document).ready(function() {
   $('.diet-compo__slider').slick({
@@ -30,6 +18,12 @@ $(document).ready(function() {
     arrows:false,
     dots:true,
     centerMode: true
+  });
+});
+
+$(document).ready(function() {
+  $('.spoiler__minisubtitle').click(function(event) {
+    $(this).toggleClass('active').next().slideToggle(300);
   });
 });
 
